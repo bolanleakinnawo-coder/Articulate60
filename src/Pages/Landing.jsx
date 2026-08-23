b7baa8142170eaee42bc264cfab98fb2199ce1bb
bolanleakinnawo-coder <bolanleakinnawo@gmail.com>
2026-08-23 18:36:39 +0100

    latest

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Nav from "../Components/landing/Nav";
 import Hero from "../Components/landing/Hero";
 import "../styles/Landingpage.css";
+import WhatYouCanDo from "../Components/landing/WhatYouCanDo";
+import HowItWorks from "../Components/landing/HowItWorks";
+import CurrentChallenge from "../Components/landing/CurrentChallenge";
 
 const Landing = () => {
   return (
     <div>
       <Nav />
       <Hero />
+      <WhatYouCanDo />
+      <HowItWorks />
+      <CurrentChallenge />
     </div>
   );
 };
